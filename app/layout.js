158290d345e87d5be1059a16f3bcb3efacb31fcb
leart-startkiwi <leart@startkiwi.com>
2024-06-04 23:38:58 +0200

    initial commit

---
+++ app/layout.js
@@ -0,0 +1,40 @@
+import Header from "./_components/Header";
+import Logo from "./_components/Logo";
+import Navigation from "./_components/Navigation";
+import { ReservationProvider } from "./_components/ReservationContext";
+import "./_styles/globals.css";
+
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const metadata = {
+  title: {
+    template: "The Wild Oasis | %s",
+    default: "Welcome / The Wild Oasis",
+  },
+  description:
+    "Luxurios cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests",
+};
+
+function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
+      >
+        <Header />
+        <div className="flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl mx-auto w-full">
+            <ReservationProvider>{children}</ReservationProvider>
+          </main>
+        </div>
+      </body>
+    </html>
+  );
+}
+
+export default RootLayout;
